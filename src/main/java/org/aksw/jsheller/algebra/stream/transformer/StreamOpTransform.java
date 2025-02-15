0f81ff6ef5633d0d4d252bbee8fd8febaf2e2b34
Claus Stadler <RavenArkadon@googlemail.com>
2025-02-16 00:59:57 +0100

    Fixed type in module name

---
+++ src/main/java/org/aksw/jsheller/algebra/stream/transformer/StreamOpTransform.java
@@ -0,0 +1,8 @@
+package org.aksw.jsheller.algebra.stream.transformer;
+
+import org.aksw.jsheller.algebra.stream.op.StreamOp;
+
+public interface StreamOpTransform
+    extends StreamOpTransformGeneric<StreamOp>
+{
+}
