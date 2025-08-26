68cfd874d88548ae2212d0cf04e2bb02decd0f69
Claus Stadler <RavenArkadon@googlemail.com>
2025-08-26 12:12:18 +0200

    wip

--- src/main/java/org/aksw/shellgebra/exec/ExecBuilderHost.java
+++ src/main/java/org/aksw/shellgebra/exec/ExecBuilderHost.java
@@ -4,6 +4,7 @@ import java.io.ByteArrayInputStream;
 import java.io.FilterInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -13,8 +14,6 @@ import java.util.List;
 import java.util.Map.Entry;
 import java.util.stream.Collectors;
 
-import org.aksw.commons.util.docker.ContainerPathResolver;
-import org.aksw.jenax.engine.qlever.SystemUtils;
 import org.aksw.shellgebra.algebra.cmd.arg.CmdArgCmdOp;
 import org.aksw.shellgebra.algebra.cmd.arg.CmdArgLiteral;
 import org.aksw.shellgebra.algebra.cmd.op.CmdOp;
@@ -22,7 +21,6 @@ import org.aksw.shellgebra.algebra.cmd.op.CmdOpExec;
 import org.aksw.shellgebra.algebra.cmd.redirect.RedirectFile;
 import org.aksw.shellgebra.algebra.cmd.redirect.RedirectFile.OpenMode;
 import org.aksw.shellgebra.algebra.cmd.transform.CmdString;
-import org.aksw.shellgebra.algebra.cmd.transform.FileMapper;
 import org.aksw.shellgebra.exec.FileWriterTaskBase.PathLifeCycle;
 import org.apache.commons.io.IOUtils;
 import org.apache.jena.riot.Lang;
@@ -35,63 +33,38 @@ import com.github.dockerjava.api.async.ResultCallback.Adapter;
 import com.github.dockerjava.api.command.AttachContainerCmd;
 import com.github.dockerjava.api.command.WaitContainerResultCallback;
 import com.github.dockerjava.api.model.AccessMode;
-import com.github.dockerjava.api.model.Bind;
 import com.github.dockerjava.api.model.Frame;
 import com.google.common.io.ByteSource;
+import com.nimbusds.jose.util.StandardCharset;
 
-public class ExecBuilderDocker {
-    private static final Logger logger = LoggerFactory.getLogger(ExecBuilderDocker.class);
 
-    // A Docker image reference consists of several components that describe where the image is stored and its identity. These components are:
-    // https://docs.docker.com/reference/cli/docker/image/tag/ - [HOST[:PORT]/]NAMESPACE/REPOSITORY[:TAG]
-    protected String imageRef;
+public class ExecBuilderHost {
+    private static final Logger logger = LoggerFactory.getLogger(ExecBuilderHost.class);
 
-    // protected String imageTag;
-    // protected List<String> cmd;
     protected CmdOp cmdOp;
-    protected List<Bind> binds;
-    protected String workingDirectory;
-    protected ContainerPathResolver containerPathResolver;
-    protected FileMapper fileMapper;
+
+    // Input task can be:
+    // - java input stream
+    // - another command / process builder
+    // - a local file
     protected FileWriterTask inputTask;
+    protected ByteSource inputSource;
 
-    // List<Bind> binds
-    public ExecBuilderDocker(String imageRef, CmdOp cmdOp, FileMapper fileMapper, ContainerPathResolver containerPathResolver) {
+
+    // protected String workingDirectory;
+
+    public ExecBuilderHost(CmdOp cmdOp) {
         super();
-        this.imageRef = imageRef;
+        // ProcessBuilder.startPipeline(null);
+        ProcessBuilder x;
         this.cmdOp = cmdOp;
-        this.fileMapper = fileMapper;
-        // this.binds = binds;
-        this.containerPathResolver = containerPathResolver;
-        // this.workingDirectory = workingDirectory;
-        // this.containerPathResolver = containerPathResolver;
     }
 
-    public static ExecBuilderDocker of(String imageRef, CmdOp cmdOp, FileMapper fileMapper) { // List<Bind> binds) {
-        ContainerPathResolver containerPathResolver = ContainerPathResolver.create();
-        return new ExecBuilderDocker(imageRef, cmdOp, fileMapper, containerPathResolver);
+    public static ExecBuilderHost of(CmdOp cmdOp) {
+        return new ExecBuilderHost(cmdOp);
     }
 
-    protected String getUserString() throws IOException {
-        int uid = SystemUtils.getUID();
-        int gid = SystemUtils.getGID();
-        String userStr = uid + ":" + gid;
-        return userStr;
-    }
-
-//    public static String buildDockerImageName(String imageName, String imageTag) {
-//        String tag = getImageTag(imageTag);
-//        String image = getImageName(imageName);
-//        String result = Stream.of(image, tag)
-//            .filter(x -> x != null)
-//            .collect(Collectors.joining(":"));
-//        return result;
-//    }
-
-    protected org.testcontainers.containers.GenericContainer<?> setupContainer(CmdOp cmdOp) throws NumberFormatException, IOException, InterruptedException {
-        String userStr = getUserString();
-        logger.info("Setting up container with UID:GID=" + userStr);
-
+    protected ProcessBuilder setupProcessBuilder(CmdOp cmdOp) throws NumberFormatException, IOException, InterruptedException {
         SysRuntime runtime = SysRuntimeImpl.forCurrentOs();
         CmdOp op = new CmdOpExec("/usr/bin/bash", new CmdArgLiteral("-c"), new CmdArgCmdOp(cmdOp));
         CmdString cmdString = runtime.compileString(cmdOp);
@@ -100,36 +73,9 @@ public class ExecBuilderDocker {
             List.of(cmdString.cmd()).stream().collect(Collectors.joining(" "))
         };
 
-
-        // String str = cmdString.scriptString();
-
-        // Path outputFolder = config.getOutputFolder();
-        // String finalImageName = QleverConstants.buildDockerImageName(imageName, imageTag);
-        // Path finalOutputFolder = ContainerPathResolver.resolvePath(containerPathResolver, outputFolder);
-
-        // String[] cmdParts = cmdString.cmd(); // cmd.toArray(new String[0]);
-        // String[] cmdStrs = new String[] {"/usr/bin/bash", "-c", str};
-
         List.of(cmdParts).stream().forEach(p -> System.out.println("[" + p + "]"));
 
-        org.testcontainers.containers.GenericContainer<?> result = new org.testcontainers.containers.GenericContainer<>(imageRef)
-            // .withWorkingDirectory(workingDirectory)
-            // .withExposedPorts(containerPort)
-            // Setting UID does not work with latest image due to
-            // error "UID 1000 already exists" ~ 2025-01-31
-            // .withEnv("UID", Integer.toString(uid))
-            // .withEnv("GID", Integer.toString(gid))
-            .withCreateContainerCmdModifier(cmd -> cmd.withUser(userStr))
-            // .withFileSystemBind(finalOutputFolder.toString(), "/data/", BindMode.READ_WRITE)
-            .withCommand(cmdParts)
-            .withLogConsumer(frame -> logger.info(frame.getUtf8StringWithoutLineEnding()))
-            // .withCommand(new String[]{"ServerMain -h"})
-            ;
-
-        for (Bind bind : fileMapper.getBinds()) {
-            result = result.withFileSystemBind(bind.getPath(), bind.getVolume().getPath(), toBindMode(bind.getAccessMode()));
-        }
-
+        ProcessBuilder result = new ProcessBuilder(cmdParts);
         return result;
     }
 
@@ -137,37 +83,36 @@ public class ExecBuilderDocker {
         return null;
     }
 
-    protected FileWriterTask execToPathInternal(Path outPath, String outContainerPath, PathLifeCycle pathLifeCycle) throws NumberFormatException, IOException, InterruptedException {
-        // Closer closer = Closer.create();
+    protected List<Process> execToProcesses() {
 
+    }
+
+    protected FileWriterTask execToPathInternal(Path outPath, PathLifeCycle pathLifeCycle) throws NumberFormatException, IOException, InterruptedException {
         List<FileWriterTask> inputTasks = new ArrayList<>();
+
+        List<ProcessBuilder> processBuilders = List.of();
+        List<Process> processes = ProcessBuilder.startPipeline(processBuilders);
+
+        Process firstProcess = processes.get(0);
+        Process lastProcess = processes.get(processes.size() - 1);
+
+        if (inputSource != null) {
+            try (OutputStream out = firstProcess.getOutputStream()) {
+                try (InputStream in = inputSource.openStream()) {
+                    in.transferTo(out);
+                }
+            }
+        }
 
         CmdOp tmpOp = cmdOp;
         if (inputTask != null) {
-//            inputTask.start();
-            Entry<Path, String> inputBind = fileMapper.allocateTempFile("byteSource", "", AccessMode.ro);
-            // Path hostPath = inputBind.getKey();
-            tmpOp = CmdOp.prependRedirect(tmpOp, new RedirectFile(inputBind.getValue(), OpenMode.READ, 0));
-//            closer.register(() -> {
-//                try { inputTask.close(); }
-//                catch (Exception e) { throw new RuntimeException(e); }
-//            });
-            // Set up a bind for the input
-            // FileWriterTask inputTask = new FileWriterTaskFromByteSource(hostPath, PathLifeCycles.namedPipe(), byteSource);
-
+            Path file = inputTask.getOutputPath();
+            tmpOp = CmdOp.prependRedirect(tmpOp, new RedirectFile(file.toAbsolutePath().toString(), OpenMode.READ, 0));
             inputTasks.add(inputTask);
         }
 
-        // SysRuntimeImpl.forCurrentOs().createNamedPipe(outPipePath);
-        CmdOp effectiveOp = CmdOp.appendRedirect(tmpOp, RedirectFile.fileToStdOut(outContainerPath, OpenMode.WRITE_TRUNCATE));
-
-        // effectiveOp = tmpOp;
-
-//        CmdString cmdString = SysRuntime.toString(effectiveOp);
-//        String[] parts = cmdString.cmd();
-
-        // If there is a byte source as a file writer then start it.
-        org.testcontainers.containers.GenericContainer<?> container = setupContainer(effectiveOp);
+        String outPathStr = outPath.toAbsolutePath().toString();
+        CmdOp effectiveOp = CmdOp.appendRedirect(tmpOp, RedirectFile.fileToStdOut(outPathStr, OpenMode.WRITE_TRUNCATE));
 
         FileWriterTask task = new FileWriterTaskFromContainer(container, outPath, pathLifeCycle, inputTasks);
 
@@ -247,7 +192,7 @@ public class ExecBuilderDocker {
     protected void runContainerWithInputStream(ByteSource byteSource, Lang lang) throws InterruptedException, IOException {
 
         logger.info("Attempting to launch container with a JVM-based input stream.");
-        org.testcontainers.containers.GenericContainer<?> container = setupContainer(null)
+        org.testcontainers.containers.GenericContainer<?> container = setupProcessBuilder(null)
             .withCreateContainerCmdModifier(cmd -> cmd
                 // .withTty(true)         // Required to keep input open
                 .withTty(false)
@@ -292,7 +237,7 @@ public class ExecBuilderDocker {
                Adapter<Frame> callback = new ResultCallback.Adapter<Frame>() {
                     @Override
                     public void onNext(Frame frame) {
-                        String msg = new String(frame.getPayload(), StandardCharsets.UTF_8);
+                        String msg = new String(frame.getPayload(), StandardCharset.UTF_8);
                         logger.info(msg);
                         super.onNext(frame);
                     }
